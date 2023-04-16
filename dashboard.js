c69e194bcd1eed2d38635f9f12f6fd43fbe0ec76
unknown <adeoluwaloveowolabi@gmail.com>
2023-04-16 18:43:12 +0100

    i have added functionality to the post button

--- dashboard.js
+++ dashboard.js
@@ -12,8 +12,8 @@ let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 // If there is no current user, redirect to the login page
 if (!currentUser) {
-    window.location.href = "index.html";
-  }
+  window.location.href = "index.html";
+}
 
 // Display the welcome message with the current user's name
 dashboardWelcome.innerHTML = `<h2>Welcome to your dashboard , ${currentUser.username}!</h2>`;
@@ -28,20 +28,27 @@ logoutBtn.addEventListener("click", () => {
 let blogTitle = document.getElementById('blogTitle');
 let blogContent = document.getElementById('blogContent');
 let postwrapper = document.getElementById('postwrapper')
-let viewwrapper = document.getElementById('viewwrapper')
+let viewwrapper = document.getElementById('posted')
+let date = new Date
+console.log(date);
 // postwrapper.style.display = 'none'
 
 function post() {
   viewwrapper.innerHTML = `
   <h1>${blogTitle.value}</h1>
   <h6>${blogContent.value}</h6>
+  <p>Posted on ${date}</p>
+  <div>
+  <button class="btn1">Like</button>
+  <button class="btn1">Unlike</button>
+  </div>
   `
 }
 
 
 function postBlog() {
   postwrapper.style.display = 'block'
-  
+
 }
 
 
