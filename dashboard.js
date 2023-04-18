ba768cfc175c2c6095dafd690cc1ede514c47ed4
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-04-18 10:54:57 +0100

    Fixed issue with view posts not updating

--- dashboard.js
+++ dashboard.js
@@ -32,13 +32,15 @@ let viewwrapper = document.getElementById('posted')
 let Liked = document.getElementsByClassName('Liked')
 let date = new Date
 // let gotten = JSON.parse(localStorage.getItem("allPosts"))
-// postwrapper.style.display = 'none'
+viewwrapper.style.display = 'none'
 let postArr = JSON.parse(localStorage.getItem("saved")) || [];
 console.log(postArr);
+showPost()
+
 function post() {
-  
+
   let postObj = {
-    // id: postArr.length+1,
+    id: postArr.length + 1,
     blogTitle: blogTitle.value,
     blogContent: blogContent.value,
     time: date,
@@ -63,32 +65,47 @@ function post() {
 function showPost() {
   if (postArr == null) {
     return;
-  }else{
+  } else {
     viewwrapper.innerHTML = ""
     postArr.forEach((element, index) => {
       viewwrapper.innerHTML += `
-          <h1>${element.blogTitle}</h1>
-          <p>${element.blogContent}</p>
-          <small>${date}</small>
+          <div>
+              <h1>${element.blogTitle}</h1>
+              <p>${element.blogContent}</p>
+              <small>${date}</small>
+          </div>
       `
-      if(element.isLiked == true){
-          viewwrapper.innerHTML += `
+      if (element.isLiked == true) {
+        viewwrapper.innerHTML += `
           <button style="background-color: tomato;" onclick="likePost(${element.id})">
               <i class="icofont-heart"></i>
           </button>
           `
-      }else{
-          viewwrapper.innerHTML += `
+      } else {
+        viewwrapper.innerHTML += `
           <button onclick="likePost(${element.id})">
-              <i class="icofont-heart"></i>
+              Like
           </button>
           `
       }
-      
-  });
+
+    });
   }
 }
-
+showPost()
+function likePost(params) {
+  let found = postArr.find(el => el.id == id);
+  if (found.isLiked == false) {
+    found.isLiked = true;
+    localStorage.setItem("saved", JSON.stringify(postArr))
+    showPost()
+  } else {
+    found.isLiked = false;
+    localStorage.setItem("saved", JSON.stringify(postArr))
+    showPost()
+  }
+  console.log(found);
+}
 
 function postBlog() {
   postwrapper.style.display = 'block'
